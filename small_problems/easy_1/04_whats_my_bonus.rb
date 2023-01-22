a82d57a111851be7ec8096d004a2723fbeb3d7de
Eli <mojojoflow@pm.me>
2023-01-22 15:27:52 -0600

    Complete Small Problems: Easy 1 - Exercise 4

---
+++ small_problems/easy_1/04_whats_my_bonus.rb
@@ -0,0 +1,20 @@
+=begin  
+
+Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. 
+If the boolean is true, the bonus should be half of the salary. 
+If the boolean is false, the bonus should be 0.
+
+=end
+
+def calculate_bonus(salary, bonus_granted)
+  if bonus_granted
+    return salary * 0.5
+  else
+    return 0
+  end
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
+
