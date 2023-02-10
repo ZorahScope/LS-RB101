82f4313766c1ab5a036629c46617a76a4e8b4f5e
Eli <mojojoflow@pm.me>
2023-02-09 19:16:07 -0600

    Swap if/else for ternary

--- rb101-rb109_small_problems/easy_2/01_how_old_is_teddy.rb
+++ rb101-rb109_small_problems/easy_2/01_how_old_is_teddy.rb
@@ -4,15 +4,10 @@
 def prompt_name
   print("Please enter a name: ")
   name = gets.chomp
-  if name.empty?
-    "Teddy"
-  else
-    name
-  end
+  name.empty? ? "Teddy" : name
 end 
 
 age = rand(20..200)
 name = prompt_name
 
 puts("#{name} is #{age} years old!")
-
