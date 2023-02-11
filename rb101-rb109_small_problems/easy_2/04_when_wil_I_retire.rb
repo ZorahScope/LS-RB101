2fc27ffdd8f3a511c9bbedb45c415f0c67bf4e30
Eli <mojojoflow@pm.me>
2023-02-11 13:52:18 -0600

    Complete Easy 2 - Problem 4

---
+++ rb101-rb109_small_problems/easy_2/04_when_wil_I_retire.rb
@@ -0,0 +1,18 @@
+# Build a program that displays when the user will retire and how many years she has to work till retirement.
+
+print("What is your age? ")
+age = gets.chomp.to_i
+
+print("At what age would you like to retire? ")
+retirement_age = gets.chomp.to_i
+
+current_year = Time.now.year
+remaining_work_years = retirement_age - age
+retirement_year = current_year + remaining_work_years
+
+message = <<-MSG
+It's #{current_year}. You will retire in #{retirement_year}.
+You have only #{remaining_work_years} years of work to go!
+MSG
+
+puts "\n" + message
